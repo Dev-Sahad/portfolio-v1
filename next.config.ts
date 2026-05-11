56e0092e5393c004f574c3d38e625dec401517af
Dev Sahad <dev.sxhd@gmail.com>
2026-05-12 00:55:00 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',      // Outputs a static 'out' folder
+  images: {
+    unoptimized: true,   // Required for static sites
+  },
+  // If your repo name is 'portfolio-v1', uncomment the next line:
+  // basePath: '/portfolio-v1', 
 };
 
 export default nextConfig;
