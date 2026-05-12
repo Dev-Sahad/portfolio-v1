0468cb37a57d647f66bacecb9fae2803ad73c74d
Dev Sahad <dev.sxhd@gmail.com>
2026-05-12 13:28:29 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',      // Outputs a static 'out' folder
   images: {
-    unoptimized: true,   // Required for static sites
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-  // If your repo name is 'portfolio-v1', uncomment the next line:
-  // basePath: '/portfolio-v1', 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
