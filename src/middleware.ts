1670b2d0a6bb101e609430a2a44af0448ef31986
msahadk12 <msahadk12@gmail.com>
2026-05-13 17:38:38 +0000

    Changes before Firebase Studio auto-run

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,87 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const { pathname, searchParams } = request.nextUrl
+  
+  if(pathname.startsWith("/auth")){
+     return NextResponse.redirect(new URL(`/`, request.url))
+  }
+
+  // Create an unmodified response
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options) {
+          // If the cookie is set, update the request's cookies
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove(name: string, options) {
+          // If the cookie is removed, update the request's cookies
+          request.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+        },
+      },
+    }
+  )
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user && pathname !== '/') {
+    const url = new URL(request.url)
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
+  if (user && (pathname === '/')) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
+  // Refresh session if expired - required for Server Components
+  await supabase.auth.getSession()
+
+  return response
 }
 
 export const config = {
@@ -14,6 +93,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    '/((?!_next/static|_next/image|favicon.ico).*)',
   ],
-};
+}
